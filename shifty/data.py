5bc1231cf3bfb0be62885667bb4a9d4a65413894
matthewjohnpayne <matthewjohnpayne@gmail.com>
2019-11-07 13:11:23 -0500

    development of data ingest code: testing INCOMPLETE: I am a bad person

--- shifty/data.py
+++ shifty/data.py
@@ -6,6 +6,7 @@
 import os, sys
 import astropy
 from astropy.io import fits
+from collections import OrderedDict
 
 # -------------------------------------------------------------------------------------
 # Any local imports
@@ -36,13 +37,12 @@ class ImageDataSet():
 
     def __init__(self, images , obs_code) :
 
-        self.images = images
-        self.obs_code = obs_code
+        self.images     = images
+        self.obs_code   = obs_code
 
 
     def generate_observatory_barycentric_positions(self):
         '''list_of_barycentric_positions_one_for_each_image'''
-        
         return [calculate_barycentric_position(image.header['obs_date'] , self.obs_code) for image in self.images]
 
     def generate_theta_coordinates(self):
@@ -92,9 +92,23 @@ class ImageLoader():
     # -------------------------------------------------------------------------------------
     # Public Methods
     # -------------------------------------------------------------------------------------
-    def get_image_data_set():
+    def get_image_data_set(self,):
         '''
             Overall image loader as envisaged by Geert & Matt over coffee
+             - Gets data from file(s)
+             - Does "cleaning"
+             - Creates ImageDataSet object
+             
+            *** STUB FUNCTION THAT WILL BE OVERWRITTEN BY CHILD CLASS ***
+            
+            Input:
+            ------
+            (1) ???? list of valid filepaths to valid fits-files ????
+            (2) option args to pass through to various sub-functions
+
+            Returns:
+            --------
+            ImageDataSet
         '''
         pass
 
@@ -137,20 +151,62 @@ class ImageLoader():
     # The methods below are for the loading of *general* fits-format data files
     # -------------------------------------------------------------------------------------
 
-    def _load_images(self, fits_file_paths):
+    def _load_images(self, file_spec_container ):
         '''
-            ...
+            Load multiple images
+             (1) Interprets file_spec_container to decide what files need to be loaded
+             - ADDITIONAL PRE-FILTERING CAN/WILL BE DONE BY CHILD METHODS
+             (2) Uses "_load_image()" to open the files
+            
+            Input:
+            ------
+            valid filepath(s) to valid fits-files
+            
+            Returns:
+            --------
+            list of astropy.io.fits.hdu.hdulist.HDUList objects
         '''
-        return
+        
+        # if file_spec_container happens to be a single valid fits file(s), then we are good to go ...
+        if isinstance(file_spec_container, str) and os.path.isfile(file_spec_container) :
+            fits_file_paths = [file_spec_container]
+        
+        # if file_spec_container happens to be an iterable containing only valid fits file(s), then we are good to go ...
+        elif isinstance(file_spec_container, (list, tuple, dict) ) and np.all( [ os.path.isfile(_) for _ in file_spec_container] ):
+            fits_file_paths = [ _ for _ in file_spec_container]
+        
+        # no other handling-methods currently in place (but CHILD can attempt some PRE-FILTERING)
+        else:
+            print('file_spec_container in *_load_images()* is of type [%r] and has the content ... \n\t %r ' % ( type(file_spec_container), file_spec_container) )
+            print(' *** At present, no method is in place to interpret this input *** ')
+            print(' ******                No files will be loaded               ******')
+            fits_file_paths = []
+        
+        # open filepaths
+        return [ self._load_image(fp) for fp in fits_file_paths ]
     
     def _load_image(self , fits_file_path):
         '''
             Load a single image
-            Basically a wrapper around astropy.fits.open
-            But with the potential for added functionality (to be added later)
+             - Currently a wrapper around astropy.fits.open
+             - With the potential for added functionality (to be added later)
+
+            Input:
+            ------
+            valid filepath to valid fits-file
+
+            Returns:
+            --------
+            lastropy.io.fits.hdu.hdulist.HDUList object
+             - [[ currently defaults to "None" if filepath invalid]]
+
         '''
-        assert os.path.isfile(fits_file_path), '%r is not a filepath' % fits_file_path
-        return fits.open(fits_file_path)
+        return fits.open(fits_file_path) if os.path.isfile(fits_file_path) else None
+
+
+
+
+
 
 
 
@@ -173,11 +229,75 @@ class TESSImageLoader(ImageLoader):
         # - Allow ourselves to use ImageLoader methods
         ImageLoader.__init__(self, local_dir=local_dir )
     
+        # - Define some important TESS-related quantities
+        self.file_spec_keywords = ['sector', 'camera', 'chip']
+        self.testing_keywords   = ['DEV', 'TEST']
+        self.obs_code            = 'C57'
+    
+    # -------------------------------------------------------------------------------------
+    # Public Methods
+    # -------------------------------------------------------------------------------------
+    def get_image_data_set(self, file_spec_container={}, cleaning_spec_dict={} ):
+        '''
+            Overall image loader as envisaged by Geert & Matt over coffee
+            - Gets data from file(s)
+            - Does "cleaning"
+            - Creates ImageDataSet object
+            
+            Input:
+            ------
+            (1) cleaning_spec_dict
+             - container with enough info to specify what files to open
+             - is passed-through-to and interpreted-by *_load_images()*
+             - [[probably a cleaner way to do this involving **kwargs, but I just want to make progress]]
+            (2) cleaning_spec_dict
+             - option args to pass through to various sub-functions
+            
+            Returns:
+            --------
+            ImageDataSet
+            '''
+        
+        # Load the data from files
+        HDUs = self._load_images( file_spec_container )
+        
+        # clean the data
+        # - [[could just ovewrite original HDUs with clean version: will leave both for now]]
+        cleanHDUs = self._clean_data( HDUs, cleaning_spec_dict )
+        
+        # create & return ImageDataSet
+        return ImageDataSet(cleanHDUs, self.obs_code)
 
     # -------------------------------------------------------------------------------------
     # The methods below are for the "CLEANING" of TESS data
     # -------------------------------------------------------------------------------------
-    def _mask_stars(self,):
+    def _clean_data(self, HDUs, **kwargs):
+        ''' 
+            Wrapper around the various "cleaning" methods below
+            - Simply provided as a means to enable simple toggling on/off of functions 
+            - Uses **kwargs to control what is/isn't evaluated
+        '''
+        # dict to hold key:function mappings
+        # - Have used ORDERED because at some point it might be important what order the funcs are used
+        cleaning_function_dict = OrderedDict(
+                                'mask'      : self._mask_stars(),
+                                'subtract'  : self._subtract_stars(),
+                                'bad_cad'   : self._remove_bad_cadences(),
+                                'scat'      : self._remove_scattered_light_problem_areas(),
+                                'strap'     : self._remove_strap_regions(),
+                                )
+                                
+        # loop over possible funcs (in order of dict)
+        # [[ NOTICE THE IMPLICIT DESIGN CHOICE THAT ALL CLEANING FUNCTIONS MUST RETURN HDUs]]
+        for key, func_to_run in cleaning_function_dict.items():
+            # run a function if it is included as True in kwargs (e.g. {'mask':True})
+            if key in kwargs and kwargs[key]:
+                HDUs = func_to_run(HDUs, **kwargs)
+
+
+        
+
+    def _mask_stars(self,HDUs, **kwargs):
         ''' 
             We want to remove stars in some way
             Barentsen & Payne discussed a simple mask: i.e. set pixels that contain stars to NaN
@@ -188,9 +308,9 @@ class TESSImageLoader(ImageLoader):
             Presumably only one of _mask_stars / _subtract_stars is required, but I am 100% certain that Holman will at least want to experiment with subtraction
             
         '''
-        pass
+        return HDUs
     
-    def _subtract_stars(self,):
+    def _subtract_stars(self,HDUs, **kwargs):
         '''
             We want to remove stars in some way
             Holman & Payne have generally assumed some form of subtraction
@@ -199,30 +319,61 @@ class TESSImageLoader(ImageLoader):
             
             Presumably only one of _mask_stars / _subtract_stars is required, but I am 100% certain that Holman will at least want to experiment with subtraction
 
-        '''
-        pass
+            Input:
+            --------
+            list HDUs
 
-    def _remove_bad_cadences(self,):
+            Returns:
+            --------
+            list HDUs
+        '''
+        return HDUs
+
+    def _remove_bad_cadences(self,HDUs, **kwargs):
         '''
             In many cases it may be most straightforward to simply eliminate entire exposures
             E.g. If they have terribly high stray-light across the entire exposure
+            
+            Input:
+            --------
+            list HDUs
+            
+            Returns:
+            --------
+            list HDUs
         '''
-        pass
+        return HDUs
 
-    def _remove_scattered_light_problem_areas(self,):
+    def _remove_scattered_light_problem_areas(self,HDUs, **kwargs):
         ''' 
             TESS has certain regions (of the chip(s)) in certain exposures that are known to have ...
             ... high levels of polluting scattered light
             We may want to completely mask these out
+            
+            Input:
+            --------
+            list HDUs
+            
+            Returns:
+            --------
+            list HDUs
         '''
-        pass
+        return HDUs
 
-    def _remove_strap_regions(self,):
+    def _remove_strap_regions(self,HDUs, **kwargs):
         '''
             TESS has certain regions (of the chip(s)) in which the backing-straps provide confusingly high signals
             We may want to completely mask these out (or perhaps flag them in some alternative manner)
+            
+            Input:
+            --------
+            list HDUs
+            
+            Returns:
+            --------
+            list HDUs
         '''
-        pass
+        return HDUs
 
 
     # -------------------------------------------------------------------------------------
@@ -231,7 +382,7 @@ class TESSImageLoader(ImageLoader):
     def _ensure_tess_subdirectory_structure_exists(self, sectorNumber, cameraNumber, chipNumber):
         '''
             Ensure subdirectory structure exists
-            Subdirectories organized as self.local_dir/sector/camera/chip
+             - Subdirectories organized as self.local_dir/sector/camera/chip
             '''
         try:
             assert os.path.isdir(self.local_dir)
@@ -293,15 +444,20 @@ class TESSImageLoader(ImageLoader):
     def _parse_download_script(self , shFile ):
         '''
             Parse a script downloaded using '_download_download_script'
+             - I.e. read it to understand the names of the files that will be downloaed and hence where we want to file them away
             
             Contents look like 16,000 rows of ...
             curl -C - -L -o tess2018292175940-s0004-1-2-0124-s_ffic.fits https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/tess2018292175940-s0004-1-2-0124-s_ffic.fits
             
-            Will output a list of fits files 
+            Will output a dictionary containing ...
+             - list of fits files
             E.g. [ 'tess2018292095940-s0004-1-1-0124-s_ffic.fits' , '']
             
-            Will also parse the fits files to understand their sector, camera, chip, ...
+             - Will also parse the fits files to understand their sector, camera, chip, ...
             E.g. [(4,1,1,)]
+            
+             - Will also regenerate a 'corrected' curlcommand to put the file into the location we want 
+            E.g. 'curl -C - -L -o /Users/matthewjohnpayne/.shifty_data/4/4/4/tess2018292105940-s0004-4-4-0124-s_ffic.fits https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/tess2018292105940-s0004-4-4-0124-s_ffic.fits'
         '''
         # read contents
         with open(shFile, 'r') as fh:
@@ -322,26 +478,70 @@ class TESSImageLoader(ImageLoader):
             returnDict['filepaths'].append(result['filepath'])
         
             # edit the curl command to redirect output to the sub-directory that we want  ...
-            try:
-                returnDict['curlCommands'].append(' '.join( *originalCurl.split()[:5], result['filepath'], *originalCurl.split()[6:] ) )
-            except:
-                print(originalCurl)
-                print(originalCurl.split())
-                print(originalCurl.split()[:5])
-                print(result['filepath'])
-                print(originalCurl.split()[6:])
-                sys.exit()
-
-        return returnDict
-    
+            returnDict['curlCommands'].append(' '.join( (*originalCurl.split()[:5], result['filepath'], *originalCurl.split()[6:] ) ) )
         
+        return returnDict
+
+
+
+    # -------------------------------------------------------------------------------------
+    # The method(s) below are for the loading of TESS fits-format data files
+    # ----------------------------------------------------- --------------------------------
+
+    def _load_images(self, file_spec_container ):
+        '''
+            This child method does PRE-FILTERING before handing off to ...
+            ... PARENT *_load_images()* method in ImageLoader
+             - PARENT knowns how to handle filepaths to fits_files
+            
+            Input:
+            ------
+            valid file_spec_container
+            
+            Returns:
+            --------
+            list of astropy.io.fits.hdu.hdulist.HDUList objects
+        '''
+        # If 'dev'/'test' is specified, then get limited test/development data-set
+        # - Will match either STRING or DICTIONARY-ENTRIES
+        # - N.B. self.testing_keywords ~ ['DEV', 'TEST']
+        if  isinstance (file_spec_container, str)  \
+            and np.any( [ _ for _ in self.testing_keywords if _ in upper(file_spec_container) ] ) \
+            or  \
+            isinstance (file_spec_container, dict) \
+            and np.any( [ _ for _ in self.testing_keywords if _ in file_spec_container and file_spec_container[_]] ):
+
+            # Use the test-data method to ensure files available locally
+            fits_file_paths = self._ensure_test_data_available_locally()
+            to_parent       = fits_file_paths
+            
+            
+
+        # If the file_spec_container contains the keywords required to specify ...
+        # ... a set of TESS fits-files, then get the required filepaths
+        # - N.B. self.file_spec_keywords = ['sector', 'camera', 'chip']
+        #
+        # [[ *** AS WRITTEN THIS WILL ONLY RETURN THE DATA FOR A SINGLE CHIP *** ]]
+        #
+        elif    isinstance (file_spec_container, dict) \
+            and np.all( [ _ in file_spec_container for _ in self.file_spec_keywords] ):
+                directory_path = os.path.join( self.local_dir, *[ '%s' % str(file_spec_container[_]) for _ in self.file_spec_keywords] )
+                to_parent      = glob.glob( os.path.join(directory_path , '*.fits'))
+
+        # Might be something that the parent knows how to parse ...
+        else:
+            to_parent      = file_spec_container
+                
+        # Hand off to PARENT to do the rest of the loading
+        return super(ImageLoader, self)._load_images( to_parent )
 
 
     # -------------------------------------------------------------------------------------
     # The methods below are for the loading of a limited, pre-defined set of TESS data
-    # Intended for the facilitation of testing
+    # Intended for testing & development
     # -------------------------------------------------------------------------------------
 
+    """
     def _load_test_data(self,):
         '''
             Load limited set of fits files for testing from local disk
@@ -353,7 +553,8 @@ class TESSImageLoader(ImageLoader):
     
         # Now that we are guaranteed that the data exists, we can open it
         # *** MJP: I Expect that the subsequent command/call will be updated (at some time in the future) to use a more general load function
-        self._load_images()
+        return self._load_images( self._define_test_data() )
+    """
     
 
     def _ensure_test_data_available_locally(self,):
@@ -362,9 +563,9 @@ class TESSImageLoader(ImageLoader):
             If they do not exist, download from MAST
         '''
         for fits_file in self._define_test_data():
-            if not os.path.isfile( os.path.join(self.local_dir, fits_file) ):
+            if not os.path.isfile(  self._fetch_tess_fits_filepath( fits_file )['filepath'] ):
                 self._download_test_data(fits_file)
-        print("\n\t All test data available locally ")
+        return self._define_test_data()
 
     def _download_test_data(self , fits_file ):
         '''
@@ -383,22 +584,28 @@ class TESSImageLoader(ImageLoader):
            Sector 4 was selected because many early teething-troubles (with pointing, etc) were overcome by this time
            
         '''
-        return [
-            "tess2018292095940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292102940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292105940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292112940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292115940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292122940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292125940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292132940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292135940-s0004-1-1-0124-s_ffic.fits",
-            "tess2018292142940-s0004-1-1-0124-s_ffic.fits"
+        return [ self._fetch_tess_fits_filepath(_)['filepath'] for _ in \
+                    [
+                    "tess2018292095940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292102940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292105940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292112940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292115940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292122940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292125940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292132940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292135940-s0004-1-1-0124-s_ffic.fits",
+                    "tess2018292142940-s0004-1-1-0124-s_ffic.fits"
+                    ]
                 ]
 
 
 
 
 
-
+class HSTImageLoader(ImageLoader):
+    '''
+        You know we'll want to do it at some point !!!
+    '''
+    pass
 
--- shifty/test_data.py
+++ shifty/test_data.py
@@ -60,6 +60,9 @@ def test_TESSImageLoader():
     # Check T object has expected attributes
     assert 'local_dir' in T.__dict__, ' local_dir not defined in T'
 
+
+    # ---- test methods related to defining storage directories -------------------------------------
+
     # Test method to derive tess_subdirectory_structure
     expectedDirectory = os.path.join(T.local_dir, str(4), str(4), str(4) )
     assert T._ensure_tess_subdirectory_structure_exists(str(4), str(4), str(4))
@@ -70,36 +73,74 @@ def test_TESSImageLoader():
     result = T._fetch_tess_fits_filepath(fits_filename)
     assert isinstance(result, dict)
     for key in ['sectorNumber','cameraNumber','chipNumber', 'filepath' ]:
-        assert key in result, '%r not in result ' %  key
+        assert key in result, \
+            '%r not in result ' %  key
     expectedFilepath = os.path.join(T.local_dir, str(4), str(1), str(1) , fits_filename)
-    assert result['filepath'] == expectedFilepath, 'filepath, %r, does not equal expectedFilepath, %r' % (result['filepath'], expectedFilepath)
+    assert result['filepath'] == expectedFilepath, \
+        'filepath, %r, does not equal expectedFilepath, %r' % (result['filepath'], expectedFilepath)
     
     
     # Test methods to get download script(s)
     sectorNumber = 4
     expectedScriptAddress = os.path.join( T.local_dir , 'tesscurl_sector_%s_ffic.sh' % sectorNumber )
-    print(expectedScriptAddress)
+    '''
     if os.path.isfile(expectedScriptAddress): os.remove(expectedScriptAddress)
     assert not os.path.isfile(expectedScriptAddress), '%r did not get removed' % expectedScriptAddress
     outFilepath = T._download_download_script(sectorNumber)
     assert  os.path.isfile(expectedScriptAddress), '%r did not get downloaded' % expectedScriptAddress
+    '''
+    outFilepath = expectedScriptAddress
 
-
-    # Test method to parse the downloaded script
+    # Test method to parse a downloaded script
     dataDict = T._parse_download_script(outFilepath)
     assert isinstance(dataDict, dict), 'not a dict '
     for key in ['fits_files','sectorNumbers','cameraNumbers','chipNumbers', 'filepaths' , 'curlCommands']:
         assert key in dataDict, '%r not in dataDict ' %  key
 
         if key == 'sectorNumbers':
-            assert np.all( [ _ == sectorNumber for _ in dataDict[key] ] ), 'sectorNumbers are wrong'
+            assert np.all( [ _ == sectorNumber for _ in dataDict[key] ] ), \
+                'sectorNumbers are wrong'
         if key == 'cameraNumbers' or key == 'chipNumbers':
-            assert np.all( [ _ in [1,2,3,4] for _ in dataDict[key] ] ), 'cameraNumbers/chipNumbers are wrong'
+            assert np.all( [ _ in [1,2,3,4] for _ in dataDict[key] ] ), \
+                'cameraNumbers/chipNumbers are wrong'
         if key in 'curlCommands':
-            print(dataDict[key][:2])
+            pass
 
-    print('test_TESSImageLoader')
 
+
+    # ----------- test methods for getting some small amount of sample/test data -----------------------
+    
+    # test whether "_define_test_data()" returns a list of filepaths
+    result = T._define_test_data()
+    expectedLength = 10
+    assert len(result) == expectedLength and np.all( [ T.local_dir in _ and ".fits" in _ for _ in result] ), \
+        '_define_test_data() returns strange results: %r ' % result
+    
+    # test whether "_ensure_test_data_available_locally" actually ensures the required test data are available
+    T._ensure_test_data_available_locally()
+    assert np.all( [ os.path.isfile(_) for _ in T._define_test_data() ] ), \
+        ' not all filepaths exist ...'
+    
+    # test the "_load_images()" method when supplied with 'DEV' option
+    # - should return list pf HDUs
+    result = T._load_images('DEV')
+    assert isinstance(result, list) and len(result) == len(T._define_test_data()), \
+        'returned result from T._load_test_data() not as expected'
+    assert np.all( [isinstance(_, astropy.io.fits.hdu.hdulist.HDUList) for _ in result]), \
+        'did not return expected object types in list'
+
+    # test the *get_image_data_set()* method on the test-data set
+    # - if we specify no cleaning functions, should be simple
+    # - should generate an ImageDataSet object
+    result = T.get_image_data_set( file_spec_container='DEV' )
+    assert isinstance(result, data.ImageDataSet ), \
+        '*get_image_data_set()* did not return an ImageDataSet'
+    assert np.all( [ _ in result.__dict__ for _ in ['images', 'obs_code'] ] ),
+        'ImageDataSet object did not have the expected attributes'
+
+
+
+    print('\t test_TESSImageLoader passed ')
 
 
 
