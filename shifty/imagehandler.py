48a2fa9d3f4a70b826286ff8dcacc4141bca3537
Mike Alexandersen (on Victoria) <mike.alexandersen@alumni.ubc.ca>
2021-03-05 22:11:38 -0500

    Restructured imagehandler, propagate header and WCS.
    Split ImageEnsemble into a DataEnsemble and DataHandler objects.
    The DataHanlder object has three DataEnsemble objects inside it,
    in image_data, shifted_data and stacked_data. This allows the
    data to always look the same nomatter which stage you
    look at. Each have a .data, .WCS and .header attribute.
    
    The WCS is now carried along and the header gets updated.
    The WCS is modified when things are shifted.
    The header is updated to contain Shifty-specific keywords, so that
    later stages of the code does not need to know what keywords are called,
    because they are always called the same thing; only the read-in stage
    needs to know what to look for and then puts it in an easy to find place.

--- shifty/imagehandler.py
+++ shifty/imagehandler.py
@@ -12,12 +12,14 @@
 # -----------------------------------------------------------------------------
 import os
 import sys
+from datetime import datetime
+import copy
 import numpy as np
 
 from astropy.io import fits
 from astropy import wcs
 from astropy.nddata import CCDData
-from ccdproc import Combiner, wcs_project
+from ccdproc import wcs_project  # , Combiner
 
 
 # -----------------------------------------------------------------------------
@@ -34,10 +36,9 @@ from shifty.downloader import Downloader
 
 class OneImage(Downloader):
     '''
-    (1)Loads a fits-file
-    (2)Reads WCS
-    (3)Calculate pixel coordinates
-    (4)Interact with 'known'?
+    (1)Loads a fits-file,
+       including data array, WCS, header and select header keywords
+    (2)Calculate pixel coordinates? (Seems unneccessary at this stage?)
 
     methods:
     --------
@@ -115,21 +116,19 @@ class OneImage(Downloader):
     # -------------------------------------------------------------------------
 
 
-class ImageEnsemble(OneImage):
+class DataEnsemble(Downloader):
     '''
     (1)Loads a list of fits-files
-    (2)Reads WCS
-    (3)Calculate pixel coordinates
-    (4)Interact with 'known'?
+    (2)Stores image data in a cube array, WCS in an array, headers in an array.
+    (3)Can reproject the data to a common WCS if neccesarry.
 
     methods:
     --------
-    loadImageAndHeader()
-    others?
+    reproject_data()
 
     main public method:
     -------------------
-    loadImageAndHeader()
+    reproject_data()
 
 
     '''
@@ -171,29 +170,45 @@ class ImageEnsemble(OneImage):
         hopefully should only ever be useful if attempting to stack images
         from different instruments; so low priority.
         '''
-        # - Allow ourselves to use OneImage methods
-        # This sets up readOneImageAndHeader as a method (even though it
-        # doesn't have to be) and sets up the local_dir for saving data.
-        OneImage.__init__(self,)
-        # Set some values
+        # - Allow ourselves to use Downloader methods
+        Downloader.__init__(self,)
         self.filename = filename
-        self.extno = extno
-        self.reprojected = False
-        self.shifted_data = None
-        self.stacked_data = None
+        if filename is not None:
+            # Set some values
+            self.extno = extno
+            self.reprojected = False
+            self.read_fits_files(verbose=verbose, **kwargs)
+        else:
+            self.extno = None
+            self.reprojected = None
+            self.data = None
+            self.WCS = None
+            self.header = None
+
+    def read_fits_files(self, verbose=False, **kwargs):
+        '''
+        Read in the files defined in self.filename
+        from extension[s] in self.extno.
+
+        input:
+        ------
+        self
+        verbose        - bool         - Print extra stuff if True
+        kwargs passed along into OneImage
+        '''
         # Do some awesome stuff!!!
         datacube = []
         wcscube = []
-        mjdcube = []
-        for i, filei in enumerate(filename):
-            exti = extno if isinstance(extno, int) else extno[i]
+        headercube = []
+        for i, filei in enumerate(self.filename):
+            exti = self.extno if isinstance(self.extno, int) else self.extno[i]
             OneIm = OneImage(filei, exti, verbose, **kwargs)
             datacube.append(OneIm.data)
             wcscube.append(OneIm.WCS)
-            mjdcube.append(OneIm.key_values['MJD_MID'])
+            headercube.append(OneIm.header)
         self.data = np.array(datacube)
         self.WCS = np.array(wcscube)
-        self.MJD = np.array(mjdcube)
+        self.header = headercube
 
     def reproject_data(self, target=0):
         '''
@@ -203,27 +218,111 @@ class ImageEnsemble(OneImage):
 
         inputs:
         -------
-        self.data
-        self.wcs
+        self.data, self.WCS, self.header
         target    - int - Index of the target image to align relative to
 
         outputs:
         --------
         self.data
+        self.wcs
+        self.header
         self.reprojected = True
         '''
-        reprojected = []
-        for i, dat in enumerate(self.data):
-            print("Reprojecting image {i}")
-            wcsi = self.WCS[i]
-            if i == target:
-                reprojected.append(CCDData(dat, wcs=wcsi, unit='adu'))
-            else:
-                reprojected.append(wcs_project(CCDData(dat, wcs=wcsi,
-                                                       unit='adu'),
-                                               self.WCS[target]))
-        self.data = np.array(reprojected)
-        self.reprojected = True
+        if self.reprojected:
+            print('Data has already been aligned and reprojected! '
+                  'Doing nothing!')
+        else:
+            for i, dat in enumerate(self.data):
+                print(f"Reprojecting image {i}")
+                if i != target:  # Don't reproject the target image, duh!
+                    # Do the reprojection
+                    reprojecti = wcs_project(CCDData(dat, wcs=self.WCS[i],
+                                             unit='adu'), self.WCS[target])
+                    # Append to list
+                    self.data[i, :, :] = reprojecti.data
+                    # Update WCS, both in self.wcs and self.header
+                    self.WCS[i] = self.WCS[target]
+                    del self.header[i]['CD?_?']  # required for update to work
+                    self.header[i].update(self.WCS[i].to_header(relax=True))
+                    # Add a comment to the header about the reprojection
+                    now = str(datetime.today())[:19]
+                    self.header[i]['COMMENT'] = (f'Data was reprojected to WCS '
+                                                 f'of file {target} at {now}')
+            self.reprojected = True
+
+
+class DataHandler():
+    '''
+    (1) Loads a list of fits-files, via DataEnsemble
+    (2) Shift data arrays by integer shifts
+    (3) Stack the data in various ways
+    (4) Save stacks
+    (5) Interact with 'known'?
+    (6) Interact with SourceExtractor?
+
+    methods:
+    --------
+    integer_shift
+    stack
+    save_stack
+
+    main public method:
+    -------------------
+    integer_shift
+    stack
+
+    '''
+    # Turning off some stupid syntax-checker warnings:
+    # pylint: disable=too-many-instance-attributes
+    # Why on earth should an object only have 7 attributes?
+    # That seems dumb. Turning off this warning.
+    # pylint: disable=too-few-public-methods
+    # I get why an object should have at least two public methods in order to
+    # not be pointless, but it's an annoying warning during dev. Turning off.
+    # pylint: disable=attribute-defined-outside-init
+    # Why should a method not be allowed to redefine an attribute?
+    # It doesn't make any sense.
+
+    def __init__(self, filename=None, extno=0, verbose=False, **kwargs):
+        '''
+        inputs:
+        -------
+        filename       - list of str  - list of filepaths to valid fits-files
+        extno          - int OR       - Extension number to use for all images
+                         list of int  - list of extension to use for each image
+        verbose        - bool         - Print extra stuff if True
+        EXPTIME        - str OR float - Exposure time in seconds
+                                      - (keyword or value)
+        MAGZERO        - str OR float - Zeropoint magnitude
+                                      - (keyword or value)
+        MJD_START      - str OR float - MJD at start of exposure
+                                      - (keyword or value)
+        GAIN           - str OR float - Gain value (keyword or value)
+        FILTER         - str          - Filter name (keyword)
+        NAXIS1         - str OR int   - Number of pixels along axis 1
+        NAXIS2         - str OR int   - Number of pixels along axis 2
+        INSTRUMENT     - str          - Instrument name (keyword)
+
+        A float/integer value can be defined for most keywords,
+        rather than a keyword name; this will use that value
+        rather than searching for the keyword in the headers.
+        INSTRUMENT and FILTER obviously can't be floats/integers,
+        so use a leading '-' to specify a value
+        rather than a keyword name to use.
+        Not yet supported: list of a keyword values/names for each image, which
+        hopefully should only ever be useful if attempting to stack images
+        from different instruments; so low priority.
+        '''
+        # Set some values
+        self.filename = filename
+        self.extno = extno
+        if filename is not None:
+            self.image_data = DataEnsemble(filename, extno, verbose, **kwargs)
+        else:
+            self.image_data = DataEnsemble()
+        self.shifted_data = DataEnsemble()
+        self.stacked_data = DataEnsemble()
+        # Do some awesome stuff!!!
 
     def integer_shift(self, shifts, padmean=False):
         '''
@@ -231,43 +330,62 @@ class ImageEnsemble(OneImage):
 
         inputs:
         -------
-        self.data
+        self.image_data
         shifts    - list of int OR array of int - Shape (N_images, 2)
 
         outputs:
         --------
-        self.shifted_data
-        self.shifted_wcs?  - It would be nice to carry the wcs along
+        self.shifted_data - data array, updated WCS and header
         '''
         # Make sure shifts is an array of integers:
         shifts = np.array(shifts).round(0).astype(int)
         # Make sure shifts are all positive (by subtracting smallest value)
         shifts[:, 0] -= shifts[:, 0].min()
         shifts[:, 1] -= shifts[:, 1].min()
-        # Find max and min shifts, to know how much to pad
+        # Find max shifts, to know how much to pad
         ymax = shifts[:, 0].max()
         xmax = shifts[:, 1].max()
+        # Populate self.shifted_data.header and .WCS by copying
+        # from self.image_data, then updating
+        self.shifted_data.header = copy.deepcopy(self.image_data.header)
+        self.shifted_data.WCS = copy.deepcopy(self.image_data.WCS)
+        # Do the shifting in a loop over each image
         shifted = []
-        for i, dat in enumerate(self.data):
+        for i, dat in enumerate(self.image_data.data):
             print(f'Shifting image {i} by {shifts[i]}')
-            pad_value = np.mean(dat) if padmean else np.nan
-            pad_size = ((shifts[i, 0], ymax - shifts[i, 0]),
-                        (shifts[i, 1], xmax - shifts[i, 1]))
+            pad_value = np.mean(dat) if padmean else np.nan  # Mean or NaN
+            pad_size = ((shifts[i, 0], ymax - shifts[i, 0]),  # Size of pad
+                        (shifts[i, 1], xmax - shifts[i, 1]))  # on 4 sides
+            # Shift the array by adding a padding around the edge
             shifted.append(np.pad(dat, pad_size, constant_values=pad_value))
-        self.shifted_data = np.array(shifted)
+            # Update WCS, both in .wcs and .header
+            self.shifted_data.WCS[i].wcs.crpix += (shifts[i, 1], shifts[i, 0])
+            del self.shifted_data.header[i]['CD?_?']  # required to update
+            updated_WCS_keys = self.shifted_data.WCS[i].to_header(relax=True)
+            self.shifted_data.header[i].update(updated_WCS_keys)
+            # Add a comment to the header about the reprojection
+            now = str(datetime.today())[:19]
+            com_str = (f'At {now} data was padded by {pad_size}')
+            self.shifted_data.header[i]['COMMENT'] = com_str
+            com_str = (f'CRPIX keywords of WCS updated to reflect padding')
+            self.shifted_data.header[i]['COMMENT'] = com_str
+        # Turn the shifted arrays into a 3D array and stick into self.
+        self.shifted_data.data = np.array(shifted)
 
     def stack(self, shifted=False, median_combine=False,
-              save_to_filename=''):
+              save_to_filename='', which_WCS='middle'):
         '''
         Stacks the data.
 
         inputs:
         -------
-        self.data
-        self.shifted_data
-        shifted           - boolean - Whether to use the shifted or plain data
-        median_combine    - boolean - Use median if True, mean otherwise
+        self.data or self.shifted_data
+        shifted           - boolean        - Whether to use the shifted
+                                           - or plain data
+        median_combine    - boolean        - Use median if True, mean otherwise
         save_to_filename  - string or None - if string, saves to that file
+        which_WCS         - string or int  - 'first', 'middle', 'last' or int
+                                           - if int, it's used as the index
 
         outputs:
         --------
@@ -276,33 +394,80 @@ class ImageEnsemble(OneImage):
         A median stack is often a little deeper than the mean stack,
         but it does not preserve photon count in a photometric way.
         So median stack is good for finding objects, mean best for photometry.
+
+        which_WCS defaults to using the WCS of the middle image.
+        It does not actually matter, though.
         '''
         print('Combining images', end='')
-        data = self.shifted_data if shifted else self.data
-        combiner = Combiner([CCDData(dati, unit='adu') for dati in data])
-        if median_combine:  # only if median is desired.
+        data = self.shifted_data.data if shifted else self.image_data.data
+        sWCS = self.shifted_data.WCS if shifted else self.image_data.WCS
+        # This method is much slower (factor 10-40) for some reason:
+#        combiner = Combiner([CCDData(dati, unit='adu') for dati in data])
+        if median_combine:  # slower; only if median is desired.
             print(' using median stacking.')
-            self.stacked_data = combiner.median_combine()
+#            self.stacked_data.data = combiner.median_combine()  # Slow method
+            self.stacked_data.data = np.median(data, 0)
         else:  # Default
             print(' using mean stacking')
-            self.stacked_data = combiner.average_combine()
+#            self.stacked_data = combiner.average_combine()  # Slow method
+            self.stacked_data.data = np.mean(data, 0)
+        if isinstance(which_WCS, int):
+            wcsidx = which_WCS
+        elif isinstance(which_WCS, str):
+            wcsidx = (0 if which_WCS.lower() == 'first'
+                      else -1 if which_WCS.lower() == 'last'
+                      else int(len(data) / 2))
+        self.stacked_data.WCS = sWCS[wcsidx]
         if save_to_filename != '':
             self.save_stack(filename=save_to_filename)
 
     def save_stack(self, filename='stack.fits'):
-        '''
-        Save a stack to a fits file.
-        '''
-        print(f'Saving stack to file {filename}')
-        hdu = fits.PrimaryHDU(self.stacked_data)
-        hdu.writeto(filename, overwrite=True)
-        print('Done!')
+        '''Save a stack to a fits file.'''
+        save_fits(self.stacked_data, filename)
+
+    def save_shifted(self, filename='shift'):
+        '''Save the shifted images to fits files.'''
+        save_fits(self.shifted_data, filename)
 
 
 # -------------------------------------------------------------------------
 # These functions really don't need to be methods, and therefore aren't.
 # No need to over-complicate things.
 # -------------------------------------------------------------------------
+
+def save_fits(DataEnsembleObject, filename='data.fits'):
+    '''
+    Save some data to [a] fits file[s].
+
+    input:
+    ------
+    DataEnsembleObject - DataEnsemble Object - contains data and header.
+    filename           - str                 - name or shared start of filename
+
+    output:
+    -------
+    one or more fits file.
+    If multiple, they are numbered *_000.fits, *_001.fits, etc.
+    '''
+    if len(DataEnsembleObject.data.shape) == 2:
+        filename = filename if '.fits' in filename else filename + '.fits'
+        hdu = fits.PrimaryHDU(data=DataEnsembleObject.data,
+                              header=DataEnsembleObject.header)
+        print(f'Saving to file {filename}')
+        hdu.writeto(filename, overwrite=True)
+    elif len(DataEnsembleObject.data.shape) == 3:
+        for i, data in enumerate(DataEnsembleObject.data):
+            filenamei = filename.replace('.fits', '')
+            filenamei += f'_{i:03.0f}.fits'
+            hdu = fits.PrimaryHDU(data=data,
+                                  header=DataEnsembleObject.header[i])
+            print(f'Saving to file {filenamei}')
+            hdu.writeto(filenamei, overwrite=True)
+    else:
+        raise ValueError('The input is not a valid DataEnsemble object.')
+
+    print('Done!')
+
 
 def readOneImageAndHeader(filename=None, extno=0, verbose=False,
                           **kwargs):
@@ -370,6 +535,15 @@ def readOneImageAndHeader(filename=None, extno=0, verbose=False,
                        'NAXIS2': 'NAXIS2',        # Pixels along axis2
                        'INSTRUMENT': 'INSTRUME',  # Instrument name
                        }
+    header_comments = {'EXPTIME': 'Exposure time [s]',
+                       'MAGZERO': 'Zeropoint magnitude',
+                       'MJD_START': 'MJD at start of exposure',
+                       'GAIN': 'Gain value',
+                       'FILTER': 'Filter letter',
+                       'NAXIS1': 'Pixels along axis1',
+                       'NAXIS2': 'Pixels along axis2',
+                       'INSTRUMENT': 'Instrument name',
+                       }
     key_values = {}
 
     # Do a loop over the kwargs and see if any header keywords need updating
@@ -384,9 +558,6 @@ def readOneImageAndHeader(filename=None, extno=0, verbose=False,
         header = han[extno].header  # Header for the extension
         # Overall header for whole mosaic, etx0:
         header0 = han[0].header  # pylint: disable=E1101 # Pylint stupid errors
-
-    # Read the WCS
-    WCS = wcs.WCS(header)
 
     # Use the defined keywords to save the values into key_values.
     # Search both headers if neccessary (using keyValue)
@@ -412,13 +583,24 @@ def readOneImageAndHeader(filename=None, extno=0, verbose=False,
             # not all the junk (telescopes usually put numbers after)
             key_values[key] = (use[1] if use[0] == '-' else
                                _find_key_value(header, header0, use)[0])
+        header[f'SHIFTY_{key}'] = (key_values[key], header_comments[key])
+        header['COMMENT'] = (f'SHIFTY_{key} added by SHIFTY'
+                             f' at {str(datetime.today())[:19]}')
+        header['COMMENT'] = f'SHIFTY_{key} derived from {use}'
 
     # Also define the middle of the exposure:
     key_values['MJD_MID'] = (key_values['MJD_START'] +
                              key_values['EXPTIME'] / 172800.0)
+    header[f'SHIFTY_MJD_MID'] = (key_values['MJD_MID'],
+                                 'MJD at middle of exposure')
+    header['COMMENT'] = (f'SHIFTY_MJD_MID added by SHIFTY'
+                         f' at {str(datetime.today())[:19]}')
+    header['COMMENT'] = (f'SHIFTY_MJD_MID derived from '
+                         f'{header_keywords["MJD_START"]} '
+                         f'and {header_keywords["EXPTIME"]}')
 
     print('{}\n'.format((key_values)) if verbose else '', end='')
-    return data, header, header0, WCS, header_keywords, key_values
+    return data, header, header0, wcs.WCS(header), header_keywords, key_values
 
 
 def _find_key_value(header1, header2, key):
